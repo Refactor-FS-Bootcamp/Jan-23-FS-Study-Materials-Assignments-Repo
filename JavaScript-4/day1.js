b11957f46cba92a7b1acc6536c1aaba2499ddbe9
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-01-23 21:33:27 +0530

    Add files via upload

---
+++ JavaScript-4/day1.js
@@ -0,0 +1,83 @@
+//console.log();
+
+// console.log('hello world')
+
+// alert('hello world')
+
+// document.write('hello world');
+
+
+//basic + DOM
+
+//varibales - container used to store data.
+
+// keyword - identifier - value;
+
+//keyword - reserved words - let,const, var.
+
+// identifier - name of the contianer.
+
+//value - data - types - datatypes.
+
+// const name = 'Vikram' 
+
+// console.log(name)
+
+// const firstNamePerson = 'hello';
+
+// const  = 'world';
+
+// name  = 'Hari' 
+
+// console.log(firstNamePerson)
+
+
+//datatypes - primitive - reference types.- object
+
+// primitve - string,number,boolean,null,undefined. - BigInt -Symbols.
+
+//typeof - 
+
+// string - '',"",``
+
+// let name = `232427862764278`;
+// console.log('The data is ', typeof name ,'-',name);
+
+//number -
+
+// let num = 0.78778;
+
+// console.log(typeof num)
+
+//true / false;
+
+// let isValid = true;
+// console.log(typeof isValid)
+
+// null- space - empty;
+
+// let data = null;
+// console.log(typeof data)
+
+//undefined.
+
+// let a;
+// console.log(typeof a)
+
+// const a  =  Symbol('foo');
+// const b  =  Symbol('foo');
+
+// console.log(a === b )
+
+/*
+assgnment
+-rules of identifers.
+-operators of javascript
+-pritive dattypes.
+*/
+
+
+
+
+
+
