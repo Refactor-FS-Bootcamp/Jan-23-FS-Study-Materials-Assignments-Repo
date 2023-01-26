d31ae3ec3a346ceef04ddbc02db1a414da1f8ecf
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-01-26 19:07:31 +0530

    Add files via upload

---
+++ JavaScript-7/app.js
@@ -0,0 +1,22 @@
+//website / webapps - 2 two roles - frontent - backends
+
+
+// ui- ux -design , font , color , 
+//frontend - code - bowser - client side work - html css
+//backend - logical - server - pr - database - ?
+
+
+// js - 1995 - brenden eich - 10 days
+//js - client side scripting lang - frontend - makeing the page dynamic and interactive withou going for the sever.
+
+// js - html - css ; browser.
+
+//0101000 -  js - file
+// let a = 10;   -10
+
+// ECOM -JS-  V8     - chrome. -DAFULT -100%
+//ECOM -JS--  sm     - firefox. - 99999
+//ECOM -JS--  nitro  - safari - 76
+
+//ECMAscript - rules and rualtion how it shoul work in browser
+//ES6 - 
