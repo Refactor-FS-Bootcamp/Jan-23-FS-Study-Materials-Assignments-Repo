4cdb0ab383616efb8a5d23c0e03135c858882668
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-01-31 19:06:09 +0530

    Add files via upload

---
+++ JavaScript-9/Week-2/day1.js
@@ -0,0 +1,134 @@
+// console.log('hello world')
+
+
+// var - scope , Hoisting;
+
+
+// scope 
+
+// var  a = 10;  //global scope:
+
+
+
+// function b(){
+//     let  a = 100; //local scope
+//     console.log(a)
+// }
+
+// b()
+// console.log(a)
+
+// if(true){
+//   let a =10;
+  
+// }
+
+// // console.log(a)
+
+
+
+// console.log(a )
+
+// var  a =10;
+// var a =100;
+
+// // console.log(a)
+// console.log(a)
+
+
+// ab()
+// function ab(){
+//    console.log('hello')
+// }
+
+
+// fun a()
+// a=undefined;
+//var
+
+
+//3 ways
+
+function fun(){
+
+}
+
+//expression
+
+// let a= function(a,b=0){
+//  console.log(a,b)
+//     let sum = a+b;
+//     return sum
+// }
+
+// console.log(a())
+
+//Arrows 
+
+// let b = a => a+a;
+     
+// // () => a+b
+// console.log(b(2))
+//  const name = 'firstName';
+
+// const app = {
+//     "b&rand" :'heloo',
+//     car:'hjsa',
+//   [name]:'hellll',
+
+
+//     detail(){
+//       console.log('helo')
+//     }
+// }
+
+// console.log(app)
+
+// app.detail()
+// console.log(app['b&rand'])
+
+//    ${}
+
+// `bj ${2+8} `
+
+// const arr = [2,3,4,4,44,34,43,3,33,34,3,3,3,34,43,34,34,34,34,34,43,4,34];
+
+//old ways
+// const item1 = arr[0];
+// console.log(item1)
+// const item2 = arr[1];
+// console.log(item2)
+// const item3 = arr[2];
+// console.log(item3)
+
+//
+
+// const [i1,i2,i3,,i4,i5,i6]=arr;
+// console.log(i1,i2,i3,i4,i5,i6)
+
+
+
+const obj = {
+    name:'hari',
+    age:788,
+    isExpe:true,
+    Skill:['html','css'],
+
+    detail(){
+        console.log('hello')
+    }
+}
+
+// console.log(obj)
+
+// const name = obj.name;
+// console.log(name)
+
+// const {name:fName,age,isExpe,detail}=obj;
+
+// console.log(fName);
+
+
+// const {Skill:[,E6]}=obj;
+
+// console.log(E6)
---
+++ JavaScript-9/day3.js
@@ -0,0 +1,92 @@
+//object - 
+// data - beha 
+//property - method
+
+
+// const car = {
+//     brand:'Audi',
+
+//     detail:function(){
+//         console.log('jsbdjck')
+//     },
+
+//     toUpperCase:function(data){
+      
+//     }
+// }
+
+// VIKRAM123@GMAIL.COM
+
+// car.brand
+// car.detail()
+
+// const name = 'vikram';
+
+
+// new String()
+
+// padStart , replaceAll()
+
+// function toUpperCase(value){
+
+// }
+
+//array method 
+
+//pop, push , shift, unshift:
+
+// const arr = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+// console.log(arr.push('Kiwi'));
+// console.log(arr.shift())
+// console.log(arr.unshift('Papaya'))
+// arr.unshift('Papaya')
+// console.log(arr)
+
+
+// const name1 =new String('hhjh');
+// console.log(name1);
+
+// console.log(name1.toString())
+
+//condition statements.
+
+
+// let coin = '1';
+// let color = 'ksnclkas';
+
+//if - else - elseif
+// if(color === 'Red'){
+//    console.log('Stop');
+// }else if(color === 'Yellow'){
+//     console.log('Ready')
+// }else if(color === 'Green'){
+//     console.log('Go')
+// }else{
+// console.log('TNA')
+// }
+// let color = 'Green';
+
+
+// switch (color){
+
+//    case 'Red':
+//         console.log('Stop')
+//         break;
+//    case 'Green':
+//          console.log('Go')
+//         break;
+//    case 'Yellow':
+//         console.log('Ready')
+//         break;
+
+//     default:
+//        console.log('TNA')
+// }
+
+
