419223c2a6a459ab07cb3dd7e744fdd403dd37d5
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-02-22 18:19:35 +0530

    Add files via upload

---
+++ my-app-4/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DemoForm from './components/DemoForm';
+
+function App() {
+  return (
+    <div className="App">
+      <DemoForm />
+    </div>
+  );
+}
+
+export default App;
