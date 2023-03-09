15f7e8b1444a7fa827ac72e68ede62951b26b7db
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-03-09 11:41:08 +0530

    Add files via upload

---
+++ hooks-2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MouseContainer from './components/MouseContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <MouseContainer />
+    </div>
+  );
+}
+
+export default App;
