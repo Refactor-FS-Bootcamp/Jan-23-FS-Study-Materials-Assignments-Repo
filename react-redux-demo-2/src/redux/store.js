3c27e92f97966edf028a7356c51c1e1d7d0248cf
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-03-17 18:12:43 +0530

    Add files via upload

---
+++ react-redux-demo-2/src/redux/store.js
@@ -0,0 +1,9 @@
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import rootReducer from "./rootReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)));
+
+export default store;
