0c49f5d991a5e3c8d05acc997598b024fe518041
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2023-04-03 18:19:58 +0530

    Add files via upload

---
+++ netflix-api-2/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+            .then(() => console.log('DB connection sucessful'))
+            .catch(err => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(8000, () => console.log("Backend server is running "));
---
+++ netflix-api-2/routes/users.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const verify = require("../verifyToken");
+
+//Update user
+router.put("/:id", verify, async(req, res) => {
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            req.body.password = CryptoJS.AES.encrypt(
+                req.body.password,
+                process.env.SECRET_KEY
+            ).toString()
+        }
+
+        try {
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.id, { $set: req.body }, { new: true }
+            )
+            res.status(200).json(updatedUser)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json("You cannot update other's account!")
+    }
+})
+
+//DELETE user
+router.delete("/:id", verify, async(req, res) => {
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        try {
+            await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("User has been deleted..")
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json("You cannot delete other's account!")
+    }
+})
+
+//GET user
+router.get("/find/:id", async(req, res) => {
+        try {
+            const user = await User.findById(req.params.id)
+            const { password, ...other } = user._doc;
+            res.status(200).json(other)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+})
+
+module.exports = router;
